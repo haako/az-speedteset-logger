50e22052278c80e4dac62a9ad68ca3fe81560027
haako <haako@computas.com>
2021-11-15 17:13:40 +0100

    add ignored file

---
+++ SpeedTestLogger/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using SpeedTest;
+using SpeedTestLogger.Models;
+
+namespace SpeedTestLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SpeedTestLogger!");
+
+            var config = new LoggerConfiguration();
+            var runner = new SpeedTestRunner(config.LoggerLocation);
+            var testData = runner.RunSpeedTest();
+            var results = new TestResult
+            {
+                SessionId = Guid.NewGuid(),
+                User = config.UserId,
+                Device = config.LoggerId,
+                Timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                Data = testData
+            };
+        }
+    }
+}
